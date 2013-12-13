49b08bbe50d74427a8cb0935eedbe03b84866fb8
Prashant Rane <prashant.rane@synerzip.com>
2013-12-13 15:12:07 +0530

    added FileManager, gitignore

---
+++ FileManager/FileManager/FileManager.h
@@ -0,0 +1,13 @@
+//
+//  FileManager.h
+//  FileManager
+//
+//  Created by Prashant Rane on 12/13/13.
+//  Copyright (c) 2013 Synerzip. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FileManager : NSObject
+
+@end
---
+++ FileManager/FileManager/FileManager.m
@@ -0,0 +1,13 @@
+//
+//  FileManager.m
+//  FileManager
+//
+//  Created by Prashant Rane on 12/13/13.
+//  Copyright (c) 2013 Synerzip. All rights reserved.
+//
+
+#import "FileManager.h"
+
+@implementation FileManager
+
+@end
